597484b8aede2961fe64cec4837638f97271f55f
Chris Roberts <code@chrisroberts.org>
2015-10-20 17:19:28 -0700

    Provide an after_configuration hook so preview mode will load files

--- lib/middleman-sparkle_docs.rb
+++ lib/middleman-sparkle_docs.rb
@@ -3,7 +3,6 @@ require 'middleman-core'
 
 # Sparkle docs extention
 class SparkleDocs < ::Middleman::Extension
-  option :my_option, 'default', 'An example option'
 
   def initialize(app, options_hash={}, &block)
     super
@@ -20,13 +19,27 @@ class SparkleDocs < ::Middleman::Extension
         FileUtils.mkdir_p(File.dirname(destination_path))
         content = File.read(doc_item).gsub('.md', '.html')
         File.write(destination_path, content)
-        builder.say_status :import, File.join('docs', item, relative_path)
+        builder.say_status :import, File.join('docs', item, relative_path) if builder
       end
     end
     # This is a crappy way to make it work, but it makes it work
     app.send(:remove_instance_variable, :@_sitemap)
     app.sitemap
     app.run_hook :ready
+  end
+
+  def after_configuration
+    %w(sfn sparkle_formation).each do |item|
+      doc_path = File.join(Gem::Specification.find_by_name(item).full_gem_path, 'docs')
+      Dir.glob(File.join(doc_path, '**', '**', '*.md')).each do |doc_item|
+        next if doc_item.end_with?('README.md')
+        relative_path = doc_item.sub(doc_path, '').sub(/^\//, '')
+        destination_path = File.join(app.config[:source], 'docs', item, relative_path)
+        FileUtils.mkdir_p(File.dirname(destination_path))
+        content = File.read(doc_item).gsub('.md', '.html')
+        File.write(destination_path, content)
+      end
+    end
   end
 
   def after_build(builder)
